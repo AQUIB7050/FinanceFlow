2a380e5ffb396ade982fa46da60858746d2f0b80
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-09-01 10:32:27 +0530

    Wrapper Component added

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-import ExpenseItem from './components/ExpenseItem';
+import Expense from "./components/Expense";
+
 
 function App() {
 
-  const expenses = [
-    {
-      title:"Car Insureance",
-      amount:1000,
-      date: new Date(2024,8,30)
-    },
-    {
-      title:"Grocery",
-      amount:700,
-      date: new Date(2024,5,21)
-
-    },
-    {
-      title:"Garden Repair",
-      amount:100,
-      date: new Date(2024,9,1)
-    }
-  ]
-
   return (   
-    <div>
-        <ExpenseItem 
-        title={expenses[0].title} 
-        amount={expenses[0].amount} 
-        date={expenses[0].date}>
-        </ExpenseItem>
-
-        <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}>
-        </ExpenseItem>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-    </div>
+    <Expense/>
   );
 }
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import './ExpenseItem.css';
 import ExpenseDate from'./ExpenseDate';
+import Card from "./Card";
+
 function ExpenseItem(props){
     
-    return (<div className="expense-item">
+    return (<Card className="expense-item">
         <div>
-            <ExpenseDate date={props.date}/>
+            <ExpenseDate date={props.expense.date}/>
         </div>
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
-            <div className="expense-item__price">${props.amount}</div>
+            <h2>{props.expense.title}</h2>
+            <div className="expense-item__price">${props.expense.amount}</div>
         </div>
 
-
-    </div>);
+    </Card>);
 }
 
 export default ExpenseItem;
