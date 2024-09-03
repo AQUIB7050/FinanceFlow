e66b654bf59fa2be107913a2c51ed41afc6fae68
Aquib Ahmed Ansari <54367316+AQUIB7050@users.noreply.github.com>
2024-09-03 21:54:28 +0530

    expense form added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 import Expense from "./components/Expenses/Expense";
+import NewExpense from './components/NewExpenses/NewExpense';
 
 
 const App = () => {
 
-  return (   
-    <Expense/>
+  const expenses = [
+    {
+      title:"Car Insureance",
+      amount:1000,
+      date: new Date(2024,8,30)
+    },
+    {
+      title:"Grocery",
+      amount:700,
+      date: new Date(2024,5,21)
+
+    },
+    {
+      title:"Garden Repair",
+      amount:100,
+      date: new Date(2024,9,1)
+    }
+  ]
+
+
+  return ( 
+    <div>
+    <NewExpense/>
+    <Expense expenses={expenses}/>
+    </div>  
   );
 }
 
